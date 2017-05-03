71d7b44b84396d2ab6c3352b52b560f03ba15700
FiReJiJi <472044804@qq.com>
2017-05-03 11:47:59 +0800

    Signed-off-by: FiReJiJi <472044804@qq.com>

---
+++ homework7/app/src/main/java/tw/com/flag/homework7/MainActivity.java
@@ -0,0 +1,27 @@
+package tw.com.flag.homework7;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+public class MainActivity extends Activity {
+    private static final String ACTIVITY_TAG="MainActivity";
+    private Button bt;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt = (Button)findViewById(R.id.bt);
+        bt.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Log.v(MainActivity.ACTIVITY_TAG, "This is Verbose.");
+                Log.d(MainActivity.ACTIVITY_TAG, "This is Debug.");
+                Log.i(MainActivity.ACTIVITY_TAG, "This is Information");
+                Log.w(MainActivity.ACTIVITY_TAG, "This is Warnning.");
+                Log.e(MainActivity.ACTIVITY_TAG, "This is Error.");
+            }
+        }); }
+}
+
